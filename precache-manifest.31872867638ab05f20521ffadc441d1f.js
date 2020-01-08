a65df5c507a29b640480bec87837b9daa7a50a32
Sunil Sandhu <sunil.sandhu87@gmail.com>
2020-01-08 16:16:56 +0100

    Add files via upload

---
+++ precache-manifest.31872867638ab05f20521ffadc441d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d755a3eaca5f4b1b9f7b7af2a0b4616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b6454a58ad0d46a502e",
+    "url": "/static/css/main.6035a9a0.chunk.css"
+  },
+  {
+    "revision": "e850d5f34ce7d0300f99",
+    "url": "/static/js/2.f2b2196d.chunk.js"
+  },
+  {
+    "revision": "1b6454a58ad0d46a502e",
+    "url": "/static/js/main.7492a841.chunk.js"
+  },
+  {
+    "revision": "0bc75928aa3bad40be23",
+    "url": "/static/js/runtime-main.c672f92e.js"
+  }
+]);
